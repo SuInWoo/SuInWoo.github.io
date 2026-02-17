ebb61ded50576aff01393885c9b8d7e861fb44a0
SuInWoo <suin@SUINui-MacBookAir.local>
2026-02-18 02:44:23 +0900

    chore: create subdirectories under _posts (CS, Project, Algorithm, Daily)

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771348802',resources: [
+    cacheName: 'chirpy-1771349827',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
